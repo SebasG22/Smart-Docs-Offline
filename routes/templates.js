fdcfdf143796a9c3065a1143570fa28b2bfbacc8
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-09 12:33:53 -0500

    update: Fix 404 Error and update templates route server

--- routes/templates.js
+++ routes/templates.js
@@ -13,6 +13,7 @@ router.post('/', function (req, res, next) {
         templateId: req.body.templateId,
         name: req.body.name,
         project: req.body.project,
+        taskType:req.body.taskType,
         icon: req.body.icon,
         content: req.body.content,
         creationDate: new Date(),
