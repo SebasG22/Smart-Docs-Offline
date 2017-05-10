d57717262e09daa2457ff65cb2db03d48a6dddd3
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-10 16:04:34 -0500

    fix

--- routes/visits.js
+++ routes/visits.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
 
-    Visit.find(
+    Visit.findOne(
         {
             siteId: req.body.siteId,
             visitId: req.body.visitId
