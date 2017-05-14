f50ef9f94c2fe6779792361c47973be52c30071c
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-14 12:30:14 -0500

    etst

--- src/libs/js/reports.js
+++ src/libs/js/reports.js
@@ -3,9 +3,10 @@ module.exports = {
     "reports":[],
     "getReports": function(){
         let reference = this;
+        console.log("Index Db" , indexDb);
         return new Promise(function(resolve,reject){
             indexDb.getReports().then(function(reportsResponse){
-                reference.reports= reportsResponse;
+                reference.reports = reportsResponse;
                 resolve(reportsResponse);
             }).catch(function(err){
                 reject(err);
