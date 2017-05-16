e844bed0152b90f4f37ba680a3085bdb18c4996e
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-16 09:16:58 -0500

    update: Save images locally

--- src/libs/js/initApplication.js
+++ src/libs/js/initApplication.js
@@ -538,7 +538,7 @@ let reportsImg = require("./reportImages");
             let promisesSaveImg = []
             do {
                 if (contProImg % 2 == 0) {
-                    this["saveAnswerImage_" + contProImg] = indexDb.addReportImages(reference.keyGenerated + subId + subIdNumber, reference.keyGenerated, this["answerImages_" + contProImg] + "]", localStorage.getItem("username"));
+                    this["saveAnswerImage_" + contProImg] = indexDb.addReportImages(reference.keyGenerated + subId + subIdNumber, reference.keyGenerated, this["answerImages_" + contProImg], localStorage.getItem("username"));
                     promisesSaveImg.push(this["saveAnswerImage_" + contProImg]);
                     subIdNumber++;
                 }
