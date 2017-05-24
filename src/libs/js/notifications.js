9faa83b69a1a2636f4c91cbb1174554cf3915e96
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-24 10:44:14 -0500

    update: Notification

--- src/libs/js/notifications.js
+++ src/libs/js/notifications.js
@@ -1,13 +1,17 @@
 module.exports = {
-    "sendNotification": function (title,body) {
+    "sendNotification": function (title, body) {
         Notification.requestPermission(function (result) {
             if (result === 'granted') {
                 navigator.serviceWorker.ready.then(function (registration) {
                     registration.showNotification(title, {
                         body: body,
-                        icon: '/img/huaweiLogo.png',
-                        vibrate: [200, 100, 200, 100, 200, 100, 200],
-                        tag: 'Smart Docs Offline'
+                        icon: '/img/huaweiLogo-192x192.png',
+                        vibrate: [200, 300, 200, 300, 200, 300, 200],
+                        tag: 'Smart Docs Offline',
+                        "actions": [
+                            { "action": "yes", "title": "Yes"},
+                            { "action": "no", "title": "No"}
+                        ]
                     });
                 });
             }
