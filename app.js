425f5745b014721d23cbc026ccf7b08c418617f2
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-24 09:30:22 -0500

    fix: Change to cloud db

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var reportImageRoutes = require("./routes/reportImages");
 
 
 var app = express();
-//mongoose.connect("Smart-Admin:huaweiDevelopers2017@ds131621.mlab.com:31621/smartdocs");
-mongoose.connect("mongodb://SmartAdmin:huaweiDevelopers2017@msdrecursos.com:27017/smartdocs");
+mongoose.connect("Smart-Admin:huaweiDevelopers2017@ds131621.mlab.com:31621/smartdocs");
+//mongoose.connect("mongodb://SmartAdmin:huaweiDevelopers2017@msdrecursos.com:27017/smartdocs");
 mongoose.Promise = global.Promise;
 
 // view engine setup
