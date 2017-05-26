2f21987027d260a9dfeba1daceecbc37e84bbede
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-25 22:08:09 -0500

    fix: Visit Get Route

--- routes/visits.js
+++ routes/visits.js
@@ -18,14 +18,14 @@ router.use('/', function (req, res, next) {
 
 router.get('/', function (req, res, next) {
     var decoded = jwt.decode(req.query.token);
-    Visit.find({author:decoded.user._id}).then(function (visits) {
+    Visit.find({ author: decoded.user._id }, function(visits){
         res.send(visits);
     });
 });
 
 router.get('/basicInformation', function (req, res, next) {
     var decoded = jwt.decode(req.query.token);
-    Visit.find({author:user,}, function(visits){
+    Visit.find({ author: user }, function (visits) {
         let visitsToSend = [];
         for (let visit of visits) {
             visitsToSend.push({ visitId: visit.visitId });
