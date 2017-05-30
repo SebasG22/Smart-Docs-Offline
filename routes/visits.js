367d7ac6049de734106cf709ee8fe16428b52d91
Sebastian Guevara <rlxsebas@gmail.com>
2017-05-29 21:30:41 -0500

    fix: update Searching by UserId

--- routes/visits.js
+++ routes/visits.js
@@ -20,7 +20,7 @@ router.use('/', function (req, res, next) {
 
 router.get('/', function (req, res, next) {
     var decoded = jwt.decode(req.query.token);
-    Visit.find({author: decoded.user._id}).then(function(visits){
+    Visit.find({author: decoded.user}).then(function(visits){
         if(!visits) {
             res.send([]);
         }
