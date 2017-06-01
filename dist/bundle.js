7f961b928e4be79c22bdacbb2a6446accd907222
Sebastian Guevara <rlxsebas@gmail.com>
2017-06-01 17:12:38 -0500

    udat

--- dist/bundle.js
+++ dist/bundle.js
@@ -13525,7 +13525,7 @@ module.exports = {
                                             
                                             myCanvas.width = 500;
                                             myCanvas.height = 500;
-                                            ctx.drawImage(img, 0, 0, 500, 600);
+                                            ctx.drawImage(img, 0, 0, 800, 800);
                                             /*
                                             ctx.font = "bold 8pt sans-serif";
                                             ctx.shadowColor = 'black';
