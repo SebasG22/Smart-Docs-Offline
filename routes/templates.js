b34b6fccbdb2e938b8bd8d5de3da9e0a81f26b05
Sebastian Guevara <rlxsebas@gmail.com>
2017-07-18 10:55:04 -0500

    feat: Adding findMyTemplates routes to get only some templates base on the user preferences

--- routes/templates.js
+++ routes/templates.js
@@ -3,9 +3,21 @@ var router = express.Router();
 const Template = require("./../models/Template");
 var jwt = require("jsonwebtoken");
 
-/*
-router.use('/', function(req, res, next) {
-    jwt.verify(req.query.token, 'SDLHW', function(err, decoded) {
+/**
+ * Method: Apply Auth Middleware to the templates routes
+ * @param {Object} Request - Request Object contains data about the user request
+ * @param {Object} Response - Using the obejct you can send a response to the user
+ * @param {function} next - Callback to continue with the next line
+ * Validate the token with SDLHW security code
+ * If the Token is valid continue finding the route, if not
+ * send a 401 code error.
+ * @author Sebastian Guevara <outs.sebastian.velasquez@huawei.com>
+ * @since Added for security reasons, when access to the API. 
+ * @version 1.0.0
+ * date  07/18/2017 DD-MM-YYYY
+ */
+router.use('/', function (req, res, next) {
+    jwt.verify(req.query.token, 'SDLHW', function (err, decoded) {
         if (err) {
             return res.status('401').json({
                 message: 'Not Authenticated',
@@ -15,11 +27,34 @@ router.use('/', function(req, res, next) {
         next();
     })
 });
-*/
+
 
 router.get('/', function (req, res, next) {
     Template.find().then(function (templates) {
         res.send(templates);
+    });
+});
+
+/**
+ * Method: Find Templates depeding on user properties like project
+ * @param {Object} Request - Request Object contains data about the user request
+ * @param {Object} Response - Using the obejct you can send a response to the user
+ * @param {function} next - Callback to continue with the next line
+ * Decode de token that the request contain
+ * Based on the user properties like project  find on the templates database and retrieve the templates that match with the filters.
+ * If no one match return and empty Array
+ * @author Sebastian Guevara <outs.sebastian.velasquez@huawei.com>
+ * @since Added for perfomance reasons, Doesn't download all the templates, only some of them based on the user preferences. 
+ * @version 1.0.0
+ * date  07/18/2017 DD-MM-YYYY
+ */
+
+router.get('/findMyTemplates', function (req, res, next) {
+    var decoded = jwt.decode(req.query.token);
+    console.log("Decoded: ", decoded);
+    Template.find({ project: { $in: decoded.project }}).then(function (templatesRetrieved) {
+        let templatesToSend = {"templates": templatesRetrieved, "total":templatesRetrieved.length};
+        res.send(templatesToSend);
     });
 });
 
@@ -28,7 +63,7 @@ router.post('/', function (req, res, next) {
         templateId: req.body.templateId,
         name: req.body.name,
         project: req.body.project,
-        taskType:req.body.taskType,
+        taskType: req.body.taskType,
         icon: req.body.icon,
         content: req.body.content,
         pdfContent: req.body.pdfContent,
